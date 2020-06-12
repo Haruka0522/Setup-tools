ccde6c4ed74fd6ced294fce0b02626d814263816
haruka.yamamoto <haruka.yamamoto@firstloop-tech.com>
2020-06-12 21:44:45 +0900

    Homebrewのインストールを追加

--- macbook-setup.sh
+++ macbook-setup.sh
@@ -2,4 +2,5 @@ git config --global user.name haruka.yamamoto
 git config --global user.email haruka.yamamoto@firstloop-tech.com
 git config --global color.ui "auto"
 
-
+# Homebrewのインストール
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
