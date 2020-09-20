25fed24f584a2650c90502398b30d655a82db279
Haruka0522 <harukapc1@gmail.com>
2020-09-20 17:08:31 +0900

    tmuxインストールがうまくいかない不具合を修正

--- Ubuntu-setup.sh
+++ Ubuntu-setup.sh
@@ -187,11 +187,13 @@ wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
 sudo apt install -y automake libevent-dev bison byacc
 sudo mkdir /usr/local/src/tmux
 sudo git clone https://github.com/tmux/tmux /usr/local/src/tmux
+cd /usr/local/src/tmux
 sudo chmod +x autogen.sh
 sudo ./autogen.sh
 sudo ./configure --prefix=/usr/local
 sudo make
 sudo make install
+cd
 
 #ペイン分割スクリプトを作成
 sudo echo "#!/bin/zsh
