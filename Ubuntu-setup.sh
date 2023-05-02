ae196e09853eb5b2364b8f0b028bf9bafddef333
Haruka0522 <harukapc1@gmail.com>
2023-05-02 16:22:30 +0900

    [Update] neovimのインストール方法を変更

--- Ubuntu-setup.sh
+++ Ubuntu-setup.sh
@@ -23,7 +23,12 @@ which zsh
 chsh
 
 #neovim
-sudo apt install neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 
 #pythonのコードチェックツール
 pip3 install -y flake8
