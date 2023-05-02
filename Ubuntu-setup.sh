13c1c5f8976d5ea24b94d2da184f373fff29b704
Haruka0522 <harukapc1@gmail.com>
2023-05-02 16:12:50 +0900

    [Update] tmuxのインストール方法を変更

--- Ubuntu-setup.sh
+++ Ubuntu-setup.sh
@@ -186,16 +186,7 @@ sudo chmod +x /usr/local/bin/docker-compose
 wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
 
 #tmuxのインストール
-sudo apt install -y automake libevent-dev bison byacc
-sudo mkdir /usr/local/src/tmux
-sudo git clone https://github.com/tmux/tmux /usr/local/src/tmux
-cd /usr/local/src/tmux
-sudo chmod +x autogen.sh
-sudo ./autogen.sh
-sudo ./configure --prefix=/usr/local
-sudo make
-sudo make install
-cd
+sudo apt -y install tmux
 
 #ペイン分割スクリプトを作成
 sudo echo "#!/bin/zsh
